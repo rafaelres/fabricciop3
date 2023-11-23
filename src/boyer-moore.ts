d630fce97014a347a57327a47be59df2a165a686
rafaelres <rafaelsiqueirinha@gmail.com>
2023-11-22 22:14:53 -0300

    filesystem

---
+++ src/boyer-moore.ts
@@ -0,0 +1,30 @@
+export function boyerMoore(pattern: string, text: string):number[]{
+  let diff = text.length - pattern.length;
+  let char_map:any = this.build_char_map(pattern);
+  let shifts:number[] = []
+  let shift:number = 0; //pattern displacement
+  while ( shift <= diff){
+      let matched:boolean = true;
+      let i = pattern.length-1;
+      console.log("Position shift:....", shift);
+      while(i>=0){
+          if(pattern.charAt(i)!= text.charAt(i+shift)){ //char mismatch
+              matched = false;
+              break;
+          }
+          --i;
+      }
+      if(matched){
+          shifts.push(shift)
+          ++shift;
+      } else {
+          if(char_map[text.charAt(i+shift)]!= undefined){
+              (i-char_map[text.charAt(i+shift)]>1)?
+                  shift= shift+(i-char_map[text.charAt(i+shift)]):
+                  ++shift;
+          } else 
+              shift += (i+1);
+      }
+  }
+  return shifts;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,11 @@
-function boyer_moore(pattern: string, text: string):number[]{
-    let diff = text.length - pattern.length;
-    let char_map:any = this.build_char_map(pattern);
-    let shifts:number[] = []
-    let shift:number = 0; //pattern displacement
-    while ( shift <= diff){
-        let matched:boolean = true;
-        let i = pattern.length-1;
-        console.log("Position shift:....", shift);
-        while(i>=0){
-            if(pattern.charAt(i)!= text.charAt(i+shift)){ //char mismatch
-                matched = false;
-                break;
-            }
-            --i;
-        }
-        if(matched){
-            shifts.push(shift)
-            ++shift;
-        } else {
-            if(char_map[text.charAt(i+shift)]!= undefined){
-                (i-char_map[text.charAt(i+shift)]>1)?
-                    shift= shift+(i-char_map[text.charAt(i+shift)]):
-                    ++shift;
-            } else 
-                shift += (i+1);                 
-        }            
-    }
-    return shifts;        
-} 
+import { boyerMoore } from "./boyer-moore"
+import fs from "fs"
+import path from "path"
+
+// path.join(__dirname, "./input.txt")
+const file = path.join(__dirname, "./input.txt")
+
+const input = fs.readFileSync(file, {
+    encoding: "utf-8", flag: "r"
+})
+console.log(input)
