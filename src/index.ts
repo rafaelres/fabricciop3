7d1d0222438107d5b0d7d9907d8eeb8d6c9f5791
rafaelres <rafaelsiqueirinha@gmail.com>
2023-11-23 21:36:08 -0300

    adiciona comentario

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
+/*
+P.3.8. Desenvolva um programa que leia um arquivo de texto e armazene cada palavra em um
+array. A seguir, utilizando o algoritmo de Boyer-Moore, determina, para cada palavra existente
+no arquivo, o número de ocorrências dessa palavra ao longo do texto. Não ignore os
+caracteres de pontuação.
+Exemplo de conteúdo do arquivo de texto:
+Eu gosto muito de Matemática. Eu Amo Matemática.
+Saída: {Eu:2, gosto: 1, muito:1, de: 1, Matemática: 2, ‘.’:2}
+
+P.3.9. Repita o Problema 3.8 mas armazenando os dados do arquivo de texto em uma única
+variável do tipo string. 
+*/
+
 import { boyerMoore } from "./boyer-moore"
-import fs, { read } from "fs"
+import fs from "fs"
 import path from "path"
 
-// Lê um arquivo de texto
 const wordResult = {}
 const readWords: string[] = []
 const file = path.join(__dirname, "./input.txt")
@@ -11,15 +23,12 @@ const input = fs.readFileSync(file, {
 })
 const words = input.match(/\p{L}+|[.,!?;]/gu)
 
-
-
 words?.forEach((word) => {
     if (readWords.includes(word)) return
     readWords.push(word)
     const searchResult = boyerMoore(word, input)
     wordResult[word] = searchResult.length
 })
-
 
 console.log(wordResult)
 
