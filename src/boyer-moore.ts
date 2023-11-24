09546a1963c8133718e49cc27922225795f94e5a
rafaelres <rafaelsiqueirinha@gmail.com>
2023-11-24 11:53:02 -0300

    eslint e refactor

--- src/boyer-moore.ts
+++ src/boyer-moore.ts
@@ -1,41 +1,38 @@
-function buildCharMap(pattern: string):any{
-    let char_map:any = {}
+function buildCharMap(pattern: string): any {
+  const char_map: any = {}
 
-    for(let i:number=0; i<pattern.length; ++i){
-        char_map[pattern.charAt(i)] = i;
-    }
+  for (let i: number = 0; i < pattern.length; ++i) {
+    char_map[pattern.charAt(i)] = i
+  }
 
-    return char_map;
+  return char_map
 }
 
+export function boyerMoore(pattern: string, text: string): number[] {
+  const diff = text.length - pattern.length
+  const char_map: any = buildCharMap(pattern)
+  const shifts: number[] = []
+  let shift: number = 0 //pattern displacement
+  while (shift <= diff) {
+    let matched: boolean = true
+    let i = pattern.length - 1
 
-export function boyerMoore(pattern: string, text: string):number[]{
-  let diff = text.length - pattern.length;
-  let char_map:any = buildCharMap(pattern);
-  let shifts:number[] = []
-  let shift:number = 0; //pattern displacement
-  while ( shift <= diff){
-      let matched:boolean = true;
-      let i = pattern.length-1;
-
-      while(i>=0){
-          if(pattern.charAt(i)!= text.charAt(i+shift)){ //char mismatch
-              matched = false;
-              break;
-          }
-          --i;
+    while (i >= 0) {
+      if (pattern.charAt(i) != text.charAt(i + shift)) {
+        //char mismatch
+        matched = false
+        break
       }
-      if(matched){
-          shifts.push(shift)
-          ++shift;
-      } else {
-          if(char_map[text.charAt(i+shift)]!= undefined){
-              (i-char_map[text.charAt(i+shift)]>1)?
-                  shift= shift+(i-char_map[text.charAt(i+shift)]):
-                  ++shift;
-          } else 
-              shift += (i+1);
-      }
+      --i
+    }
+    if (matched) {
+      shifts.push(shift)
+      ++shift
+    } else {
+      if (char_map[text.charAt(i + shift)] != undefined) {
+        i - char_map[text.charAt(i + shift)] > 1 ? (shift = shift + (i - char_map[text.charAt(i + shift)])) : ++shift
+      } else shift += i + 1
+    }
   }
-  return shifts;
+  return shifts
 }
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,48 @@
 /*
-P.3.8. Desenvolva um programa que leia um arquivo de texto e armazene cada palavra em um
-array. A seguir, utilizando o algoritmo de Boyer-Moore, determina, para cada palavra existente
-no arquivo, o número de ocorrências dessa palavra ao longo do texto. Não ignore os
-caracteres de pontuação.
-Exemplo de conteúdo do arquivo de texto:
-Eu gosto muito de Matemática. Eu Amo Matemática.
-Saída: {Eu:2, gosto: 1, muito:1, de: 1, Matemática: 2, ‘.’:2}
+    P.3.8.
+    Desenvolva um programa que leia um arquivo de texto e armazene cada palavra em um array.
+    A seguir, utilizando o algoritmo de Boyer-Moore, determina, para cada palavra existente no arquivo, o número de ocorrências dessa palavra ao longo do texto.
+    Não ignore os caracteres de pontuação.
 
-P.3.9. Repita o Problema 3.8 mas armazenando os dados do arquivo de texto em uma única
-variável do tipo string. 
+    Exemplo:
+        Entrada: Eu gosto muito de Matemática. Eu Amo Matemática.
+        Saída: { Eu: 2, gosto: 1, muito:1, de: 1, `Matemática`: 2, `.`:2}
+
+    P.3.9.
+    Repita o Problema 3.8 mas armazenando os dados do arquivo de texto em uma única variável do tipo string.
 */
 
 import { boyerMoore } from "./boyer-moore"
 import fs from "fs"
 import path from "path"
 
-const wordResult = {}
+// Objeto que armazena o resultado final
+const wordResult: Record<string, number> = {}
+
+// Lista de palavras já lidas, para não executar o algoritmo de Boyer-Moore mais de uma vez para a mesma palavra
 const readWords: string[] = []
-const file = path.join(__dirname, "./input.txt")
-const input = fs.readFileSync(file, {
-    encoding: "utf-8", flag: "r"
-})
-const words = input.match(/\p{L}+|[.,!?;]/gu)
+
+// Encontra o caminho absoluto do arquivo de texto
+const filePath: string = path.join(__dirname, "./input.txt")
+
+// Lê o conteúdo do arquivo e armazena em uma variável do tipo string
+const text: string = fs.readFileSync(filePath, { encoding: "utf-8" })
+
+// Separa todas as palavras presentes no texto, incluindo as pontuações
+const words = text.match(/\p{L}+|[.,!?;]/gu)
 
 words?.forEach((word) => {
-    if (readWords.includes(word)) return
-    readWords.push(word)
-    const searchResult = boyerMoore(word, input)
-    wordResult[word] = searchResult.length
+  // Se a palavra já foi lida, interrompe a execução
+  if (readWords.includes(word)) return
+
+  // Se a palavra já foi lida, adiciona ela ao array de palavras lidas
+  readWords.push(word)
+
+  // Executa o algoritmo de Boyer-Moore para encontrar os indicies de ocorrência da palavra dentro do texto
+  const searchResult = boyerMoore(word, text)
+
+  // Adiciona a palavra e o número de ocorrências no objeto de resultado
+  wordResult[word] = searchResult.length
 })
 
 console.log(wordResult)
-
-
